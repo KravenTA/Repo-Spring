2b8b15dba89f92e8bdade2741d81592bec40ed13
KravenTA <ythommela@miumg.edu.gt>
2025-05-29 21:12:09 -0600

    Primer commit de este Repo

---
+++ src/main/java/umg/programacionIII/Repo_Spring/RepoSpringApplication.java
@@ -0,0 +1,13 @@
+package umg.programacionIII.Repo_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoSpringApplication.class, args);
+	}
+
+}
